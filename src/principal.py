e8a345c474227c535070fee785c4482f769010d9
dudalbuquerque <157427059+dudalbuquerque@users.noreply.github.com>
2025-02-03 19:00:14 -0300

    Update principal.py

--- src/principal.py
+++ src/principal.py
@@ -1,14 +1,22 @@
 import streamlit
-from src import mybooks, suggest
+from src import mybooks, suggested, teste, searchbook
+
 
 def main():
-    tabs = streamlit.tabs(["Meus Livros", "Lista de livros sugeridos"])
+    # Inicializa o estado do menu como sempre expandido
+    streamlit.session_state.menu_expanded = True
+
+    # Exibe diretamente o menu
+    teste.menu_expanded()
+
+    tabs = streamlit.tabs(["Meus Livros", "Lista de livros sugeridos", "Buscar Livros"])
     
     with tabs[0]:
         mybooks.show_books()
         mybooks.add_book()
 
     with tabs[1]:
-        suggest.show_books_suggested()
-    
-    suggest.suggest_books()
+        suggested.show_books_suggested()
+
+    with tabs[2]:
+        searchbook.suggest_books()
