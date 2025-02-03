5b11f71a8112a09dbe02ee7ec8f34048e1fd1a02
dudalbuquerque <157427059+dudalbuquerque@users.noreply.github.com>
2025-02-03 19:02:39 -0300

    Create sidebar.py

---
+++ src/sidebar.py
@@ -0,0 +1,16 @@
+import streamlit
+from src import initialize
+
+# Tela Principal com Sidebar
+def menu_expanded():
+    with streamlit.sidebar:
+        streamlit.title("Menu")
+
+        c1, c2, _= streamlit.columns([1, 1, 1])
+        with c1:
+            streamlit.write(f"{initialize.streamlit.session_state.username}")
+        
+        with c2:
+            if streamlit.button("Logout", type="primary", use_container_width=True):                 
+                initialize.streamlit.session_state.page = "Login"                 
+                streamlit.rerun()
