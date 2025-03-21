b73c51bc0dedbca173415d15e6ef7b06fafe8502
Eduarda Albuquerque <157427059+dudalbuquerque@users.noreply.github.com>
2025-03-21 12:59:12 -0300

    Update and rename sidebar.py to my_information.py

---
+++ source/my_information.py
@@ -0,0 +1,19 @@
+import os
+import sys
+import streamlit as st
+from source.initialize import session_state, book_user
+
+def my_information():
+    amount = book_user.book_count(st.session_state.id)
+    st.markdown(f"#### Olá {st.session_state.username}")
+    st.write(" ")
+    st.markdown(f"_Sua estante de livros atualmente possui **{amount[0]+amount[1]}** livros._")
+    st.markdown(f"_Quantidade de livros lidos: **{amount[0]}**_")
+    st.markdown(f"_Quantidade de livros sugeridos/não lidos: **{amount[1]}**_")
+    st.write(" ")
+    st.write(" ")
+
+
+    if st.button("Logout", type="primary", use_container_width=True):
+        st.session_state.page = "Login"
+        st.rerun()
