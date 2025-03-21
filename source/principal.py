69b4cbc61116af9540aedd977cbfd6016026450f
Eduarda Albuquerque <157427059+dudalbuquerque@users.noreply.github.com>
2025-03-21 12:55:56 -0300

    Update principal.py

--- source/principal.py
+++ source/principal.py
@@ -1,48 +1,20 @@
-import streamlit
-from source.initialize import *
+import streamlit as st
+from source.initialize import session_state
+from source.mybooks import show_books
+from source.suggested import show_books_suggested
+from source.searchbook import suggest_books
+from source.my_information import my_information
 
 def main():
-    if streamlit.session_state.id:
-        streamlit.session_state.menu_expanded = True
-    
-    # Se o usuário não estiver logado (ou fez logout), redireciona para a página de login
-    if not streamlit.session_state.id or streamlit.session_state.page == "Login":
-        streamlit.session_state.menu_expanded = False
-        
-        # A página será "Login", mas o fluxo não tentará carregar outras páginas
-        return
+    """Página principal com navegação e opção de logout na barra lateral."""
+    tab1, tab2, tab3, tab4 = st.tabs(["Meus Livros", "Lista de livros sugeridos", "Buscar Livro", "Minhas informações"])
 
-    if streamlit.session_state.menu_expanded:
-        pages = {
-            "ReadmoreAI": [
-                streamlit.Page("source/mybooks.py", title="Meus Livros"),
-                streamlit.Page("source/suggested.py", title="Lista de livros sugeridos"),
-                streamlit.Page("source/searchbook.py", title="Buscar Livro"),
-            ],
-        }
-
-        pg = streamlit.navigation(pages)
-        pg.run()
-
-        with streamlit.sidebar:
-            c1, _, c3 = streamlit.columns([1, 1, 1])
-            with c1:
-                streamlit.write(f"{streamlit.session_state.username}")
-
-            with c3:
-                if streamlit.button("Logout", type="primary", use_container_width=True): 
-                    # Resetar todas as variáveis de estado
-                    streamlit.session_state.book_input = False
-                    streamlit.session_state.show_stars = False
-                    streamlit.session_state.book_assessment = 0
-                    streamlit.session_state.id = ''
-                    streamlit.session_state.username = ''
-                    streamlit.session_state.validation = ''
-                    streamlit.session_state.menu_expanded = False
-                    streamlit.session_state.clicked_add = ''
-                    streamlit.session_state.clicked_book = ''
-                    streamlit.session_state.page = "Login"  # Definir a página de login explicitamente
-
-                    # Atualiza a interface e reinicia o fluxo para login
-                    streamlit.rerun()
+    with tab1:
+        show_books()
+    with tab2:
+        show_books_suggested()
+    with tab3:
+        suggest_books()
+    with tab4:
+        my_information()
 
