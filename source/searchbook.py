bc0bc5143910df318a73e6f4e1c7e83470b2db76
Adriana Melcop <adrianatheilmc@gmail.com>
2025-04-02 14:06:20 -0300

    Update searchbook.py

--- source/searchbook.py
+++ source/searchbook.py
@@ -3,7 +3,10 @@ from source import mybooks, suggested
 from source.initialize import session_state, model, book_user
 
 def display_book_suggested(book):
-    """Exibe um livro sugerido com imagem, nome, autor e sinopse."""
+    """
+    Exibe um livro sugerido com imagem, nome, autor e sinopse.
+    """
+    # Estilo personalizado para centralizar os elementos
     st.markdown(
         """
         <style>
@@ -18,30 +21,37 @@ def display_book_suggested(book):
         """,
         unsafe_allow_html=True,
     )
+    
     with st.expander("Livro sugerido", expanded=True):
-        col1, col2 = st.columns([1, 2])
+        col1, col2 = st.columns([10, 1])
         with col1:
-            book_img_url = mybooks.get_book_image(book[0])
-            st.image(book_img_url, width=200)
-        with col2:
-            st.write(f"**Nome:** {book[0]}")
-            st.write(f"**Autor:** {book[1]}")
-            st.write(f"**Sinopse:** {book[2]}")
-            col1, col2 = st.columns(2)
-            with col1:
-                if st.button("Adicionar a livros sugeridos", use_container_width=True, key=f"add_suggested_{book[0]}"):
-                    st.session_state.clicked_add = book
-            with col2:
-                if st.button("Voltar", use_container_width=True):
-                    st.session_state.clicked_add = ""
-                    st.rerun()
+            st.markdown('<div class="expander-content">', unsafe_allow_html=True)
+            c1, c2 = st.columns([1, 2]) # Cria duas colunas
 
-def gen_book(no_filter, author, genre, title):
-    """Gera a sugestão de um livro com base nos filtros informados."""
+            with c1:
+                book_img_url = mybooks.get_book_image(book[0])
+                st.image(book_img_url, width=200) # Exibe a imagem do livro na primeira coluna
+            with c2:
+                st.write(f"**Nome:** {book[0]}")    # Nome do livro
+                st.write(f"**Autor:** {book[1]}")   # Autor
+                st.write(f"**Sinopse:** {book[2]}") # Sinopse, todas na segunda coluna
+
+                if st.button("Adicionar a livros sugeridos", use_container_width=True, key=f"add_suggested_{book[0]}"):
+                        st.session_state.clicked_add = book
+                        st.session_state.clicked_book_suggest = ''
+
+        with col2:
+            if st.button("❌", key="close_suggestion"):
+                st.session_state.clicked_book_suggest = ''
+                st.session_state.clicked_add = ''
+                st.rerun()         
+        st.markdown('</div>', unsafe_allow_html=True)
+
+def gen_book(none, author, genre, title):
     book_list = book_user.books_list(st.session_state.id, genre)
-    if no_filter:
+    if none:
         if not book_list:
-            suggested_text = model.generate_content(
+            suggested = model.generate_content(
                 """
                 Sugira apenas um livro interessante para leitura. 
                 Liste da seguinte forma, sem destaques ou formatação especial: Nome do Livro - Nome do Autor - Sinopse do Livro. 
@@ -50,7 +60,7 @@ def gen_book(no_filter, author, genre, title):
                 """
             ).text.strip()
         else:
-            suggested_text = model.generate_content(
+            suggested = model.generate_content(
                 f"""
                 Sugira apenas um livro que não esteja na lista de nomes e notas: {book_list}. 
                 Liste no seguinte formato, sem destaques ou formatação especial: Nome do Livro - Nome do Autor - Sinopse do Livro. 
@@ -60,7 +70,7 @@ def gen_book(no_filter, author, genre, title):
             ).text.strip()
     elif author:
         if not book_list:
-            suggested_text = model.generate_content(
+            suggested = model.generate_content(
                 f"""
                 Sugira apenas um livro escrito por {author}. 
                 Liste no seguinte formato, sem destaques ou formatação especial: Nome do Livro - Nome do Autor - Sinopse do Livro. 
@@ -69,7 +79,7 @@ def gen_book(no_filter, author, genre, title):
                 """
             ).text.strip()
         else:
-            suggested_text = model.generate_content(
+            suggested = model.generate_content(
                 f"""
                 Sugira apenas um livro escrito por {author} que não esteja na lista de nomes e notas: {book_list}. 
                 Liste no seguinte formato, sem destaques ou formatação especial: Nome do Livro - Nome do Autor - Sinopse do Livro. 
@@ -79,80 +89,91 @@ def gen_book(no_filter, author, genre, title):
             ).text.strip()
     elif genre:
         if not book_list:
-            suggested_text = model.generate_content(
+            suggested = model.generate_content(
                 f"""
-                Sugira apenas um livro do gênero {genre}. 
+                Sugira apenas m livro do gênero {genre}. 
                 Liste no seguinte formato, sem destaques ou formatação especial: Nome do Livro - Nome do Autor - Sinopse do Livro. 
                 A sinopse deve ser clara e envolvente, sem revelar spoilers importantes. 
                 Não utilize caracteres especiais.
                 """
             ).text.strip()
         else:
-            suggested_text = model.generate_content(
+            suggested = model.generate_content(
                 f"""
                 Sugira apenas um livro do gênero {genre} que não esteja na lista de nomes e notas: {book_list}. 
                 Liste no seguinte formato, sem destaques ou formatação especial: Nome do Livro - Nome do Autor - Sinopse do Livro. 
                 A sinopse deve ser clara e envolvente, sem revelar spoilers importantes. 
                 Não utilize caracteres especiais.
                 """
-            ).text.strip()
+            ).text.strip()        
     elif title:
-        suggested_text = model.generate_content(
+        suggested = model.generate_content(
             f"""
-            Sugira apenas um livro que não esteja na lista de nomes e notas: {book_list} e que tenha um título semelhante ou igual a {title}. 
+            Retorne o livro com o título {title}. 
             Liste no seguinte formato, sem destaques ou formatação especial: Nome do Livro - Nome do Autor - Sinopse do Livro. 
             A sinopse deve ser clara e envolvente, sem revelar spoilers importantes. 
             Não utilize caracteres especiais.
             """
         ).text.strip()
         
-    suggested_parts = suggested_text.split(" - ")
-    st.session_state.clicked_book_suggest = suggested_parts
+    suggested = suggested.split(" - ")
+    st.session_state.clicked_book_suggest = suggested
     st.rerun()
 
 def suggest_books():
-    """Interface principal para sugerir livros com base na entrada do usuário."""
+    """
+    Interface principal para sugerir livros com base no gênero informado pelo usuário.
+    """
     st.markdown("## Buscar Livros")
+
+
+    # Exibe livro sugerido se houver um salvo na sessão
     if st.session_state.clicked_book_suggest:
         display_book_suggested(st.session_state.clicked_book_suggest)
     
+    # Adiciona livro à lista de sugeridos se houver um marcado para adição
     if st.session_state.clicked_add:
         suggested.add_db_book_suggested(st.session_state.clicked_add)
-        st.session_state.clicked_add = ""
+        st.session_state.clicked_add = ''
         st.rerun()
+
+    
     
     option = st.selectbox(
-        "Deseja pesquisar por algo específico?",
-        ("Nenhum", "Autor/Autora", "Gênero", "Título")
+        "Deseja pesquisa por algo específico? ",
+        ("Nenhum","Autor/Autora", "Gênero", "Título"),
     )
+
     if option == "Nenhum":
         if st.button("Sugerir livro", use_container_width=True, type="primary"):
             gen_book(True, None, None, None)
+    
     elif option == "Autor/Autora":
-        name_author = st.text_input("Digite o nome do autor ou autora:", placeholder="...")
+        name_author = st.text_input("Digite o nome do autor ou autora: ", placeholder="...")
         if st.button("Pesquisar autor/autora", use_container_width=True, type="primary"):
             if name_author:
                 gen_book(None, name_author, None, None)
             else:
                 st.warning("Digite o nome do autor/autora do livro.")    
+    
     elif option == "Gênero":
         option_genre = st.selectbox(
-            "Selecione o gênero:",
-            (
-                "Romance", "Conto", "Fantasia", "Ficção Científica", "Terror/Horror", 
-                "Policial/Detetivesco", "Aventura", "Distopia/Utopia", "Romance Histórico", 
-                "Biografia", "Autobiografia", "Diário/Cartas", "Poesia", "Tragédia", 
-                "Comédia", "Drama", "Fábula", "Lenda", "Crônica", "Suspense/Thriller", "Mistério"
+        "Deseja pesquisa por algo específico? ",
+            (        
+            "Romance", "Conto", "Fantasia", "Ficção Científica", "Terror/Horror", 
+            "Policial/Detetivesco", "Aventura", "Distopia/Utopia", "Romance Histórico", 
+            "Biografia", "Autobiografia", "Diário/Cartas", "Poesia", "Tragédia", 
+            "Comédia", "Drama", "Fábula", "Lenda", "Crônica", "Suspense/Thriller", "Mistério"
             )
         )
+
         if st.button("Sugerir livro", use_container_width=True, type="primary"):
            gen_book(None, None, option_genre, None)
+
     elif option == "Título":
-        title_input = st.text_input("Digite o título do livro:", placeholder="...")
+        title = st.text_input("Digite o título do livro: ", placeholder="...")
         if st.button("Pesquisar livro", use_container_width=True, type="primary"):
-            if title_input:
-                gen_book(None, None, None, title_input)
+            if title:
+                gen_book(None, None, None, title)
             else:
                 st.warning("Digite o título do livro.")
-
-suggest_books()
